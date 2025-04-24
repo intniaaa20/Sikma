4de999bab79ce84cc39920426789484fc67994b7
intniaaa20 <10231029@student.itk.ac.id>
2025-04-24 16:28:14 +0800

    Delete resources directory

--- resources/views/filament/pages/tentang-kami.blade.php
+++
@@ -1,8 +0,0 @@
-<x-filament::page>
-    <div class="space-y-4">
-        <h2 class="text-2xl font-bold">Tentang Website</h2>
-        <p>
-            Ini adalah halaman tentang kami. Website ini dibangun dengan Laravel dan Filament.
-        </p>
-    </div>
-</x-filament::page>
